1da9a72941fdb1d4ddd6a65ed690227bfd800c98
jelie-05 <jeremialieks@gmail.com>
2025-10-15 21:17:36 +0200

    add visualization options into launch file

--- bgt60tr13c_driver/launch/radar.launch.py
+++ bgt60tr13c_driver/launch/radar.launch.py
@@ -4,6 +4,7 @@ from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.parameter_descriptions import ParameterValue
+from launch.conditions import IfCondition
 
 def generate_launch_description():
     # default to the packaged YAML, but allow override: params:=/path/to/other.yaml
@@ -23,6 +24,12 @@ def generate_launch_description():
     port_arg = DeclareLaunchArgument("port", default_value='')
     port = LaunchConfiguration("port")
 
+    vis_arg = DeclareLaunchArgument(
+        'vis',
+        default_value='false',   # or 'true' if you want it on by default
+        description='Run the visualization node (true/false)'
+    )
+    vis = LaunchConfiguration('vis')
 
     node = Node(
         package='bgt60tr13c_driver',
@@ -34,8 +41,20 @@ def generate_launch_description():
                     ],
     )
 
+    vis_node = Node(
+        package='bgt60tr13c_driver',
+        executable='raw_frame_viz_cmap',
+        name='raw_frame_viz_cmap',
+        output='screen',
+        parameters=[],
+        condition=IfCondition(vis), 
+    )
+
+
     return LaunchDescription([
         params_arg,
         port_arg,
+        vis_arg,
         node,
+        vis_node
     ])
